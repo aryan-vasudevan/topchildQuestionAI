9c7da8342a67b283be94045f51c4f7c216810f63
Aryan <vasudevan.aryan@gmail.com>
2024-10-13 13:04:22 -0400

    Connect excel file.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import openpyxl
+
+path = "questions.xlsx" # path to the excel file
+wb = openpyxl.load_workbook(path) # load the workbook
+
+sheet = wb.active  
+  
+x1 = sheet['A1']  
+x2 = sheet['A2']  
+  
+print("The first cell value:",x1.value)  
+print("The second cell value:",x2.value)  
+
 
