c8679a4deb27bf1c8a04b5d760892a33d8bbf6f1
Aryan <vasudevan.aryan@gmail.com>
2024-10-14 12:54:28 -0400

    Connect openai.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import openpyxl
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
+# Excel file
 path = "questions.xlsx" # path to the excel file
 wb = openpyxl.load_workbook(path) # load the workbook
 
 sheet = wb.active  
-  
-x1 = sheet['A1']  
-x2 = sheet['A2']  
-  
-print("The first cell value:",x1.value)  
-print("The second cell value:",x2.value)  
 
+# API
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
 
