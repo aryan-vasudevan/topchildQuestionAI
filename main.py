62c466989ec821e4e0c3af98725706d0bc2b7423
Aryan <vasudevan.aryan@gmail.com>
2024-10-15 17:53:49 -0400

    Read questions.

--- main.py
+++ main.py
@@ -14,16 +14,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
-
-print(completion.choices[0].message)
-
+questions = []
+for cell in sheet['B']:
+    if cell.value != None and cell != sheet['B1']:
+        questions.append(cell.value)
