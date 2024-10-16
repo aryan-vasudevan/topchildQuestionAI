50b41ac73c0455be1e3f6a29e4c16ee01a929ad6
Aryan <vasudevan.aryan@gmail.com>
2024-10-16 19:25:59 -0400

    Read one row at a time.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Question():
         self.question = question
         self.answerChoices = answerChoices
         self.correctAnswer = correctAnswer
+
 # Excel file
 path = "questions.xlsx" # path to the excel file
 wb = openpyxl.load_workbook(path) # load the workbook
@@ -20,6 +21,10 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 questions = []
-for cell in sheet['B']:
-    if cell.value != None and cell != sheet['B1']:
-        questions.append(cell.value)
+for row in sheet.iter_rows(min_row=2, min_col=2, max_row=20, max_col=8):
+    question = row[0].value
+    answerChoices = [cell.value for cell in row[1:5]]
+    correctAnswer = row[5].value
+    questions.append(Question(question, answerChoices, correctAnswer))
+
+print(questions[0].question, questions[0].answerChoices, questions[0].correctAnswer)
