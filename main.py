831413b6f4716dc7cb02b2022be3b2e56c3c96d5
Aryan <vasudevan.aryan@gmail.com>
2024-10-18 17:15:48 -0400

    Clean up.

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import openpyxl
-from openai import OpenAI
 import os
+from openai import OpenAI
 from dotenv import load_dotenv
 from pydantic import BaseModel
 
+# API
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PROMPT = os.getenv("PROMPT")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+# Question model for structured outputs
 class Question(BaseModel):
-    question: str
+    questionText: str
     answerChoices: list[str]
     correctAnswer: str
 
-class QuestionList(BaseModel):
-    questions: list[Question]
-
-def getNewQuestions(sampleQuestion):
+# Get new question
+def getNewQuestion(sampleQuestion):
     completion = client.beta.chat.completions.parse(
         model="gpt-4o-2024-08-06",
         messages=[{"role": "system", "content": PROMPT},
@@ -21,8 +27,7 @@ def getNewQuestions(sampleQuestion):
     )
 
     output = completion.choices[0].message.parsed
-    print(output)
-    print(type(output))
+    return output
 
 # Excel file
 path = "questions.xlsx" # path to the excel file
@@ -30,19 +35,13 @@ wb = openpyxl.load_workbook(path) # load the workbook
 
 sheet = wb.active
 
-# API
-load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-PROMPT = os.getenv("PROMPT")
-client = OpenAI(api_key=OPENAI_API_KEY)
-
-questions = []
+# Read questions
+sampleQuestions = []
 for row in sheet.iter_rows(min_row=2, min_col=2, max_row=20, max_col=8):
-    question = str(row[0].value)
+    questionText = str(row[0].value)
     answerChoices = [str(cell.value) for cell in row[1:6]]
     correctAnswer = str(row[6].value)
 
-    questions.append(Question(question=question, answerChoices=answerChoices, correctAnswer=correctAnswer))
+    sampleQuestions.append(Question(questionText=questionText, answerChoices=answerChoices, correctAnswer=correctAnswer))
 
-getNewQuestions(questions[0])
-
+getNewQuestion(sampleQuestions[0])
